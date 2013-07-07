ac0c140bfb74bb33d03ad2dd509be7a97ce0c97f
Stefan Fiedler <Stefan.M.Fiedler@gmail.com>
2013-07-07 20:28:41 +0000

    changed index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fs = require ('fs');
+
+	buf.toString = new Buffer (fs.readFileSync("index.html","utf-8"));
+
+  	response.send();
 });
 
 var port = process.env.PORT || 5000;
