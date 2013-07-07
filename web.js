6acd172ac2989704c40a1ca37e1ac2262c5487c4
Stefan Fiedler <Stefan.M.Fiedler@gmail.com>
2013-07-07 20:47:07 +0000

    changed index.html

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var fs = require ('fs');
-	var buff = new Buffer ();
-  	response.send(buff.toString('utc8', fs.readFileSync("index.html")));
+	var buffer = new buffer ();
+  	response.send(buffer.toString('utc8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
